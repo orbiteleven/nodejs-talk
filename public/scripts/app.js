c575209a1d07537ea5de0f70b9a2f98eb076c594
Damon Toal-Rossi <damontr@gmail.com>
2014-05-07 02:41:18 -0600

    Stylistic changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,19 +3,30 @@ var io = require('socket.io-client');
 
 var socket = io.connect('http://localhost:3000/');
 
-function addMessage(msg) {
-  $('#chat-window ul').append($('<li>').text(msg));
+function addMessage(msg, mine) {
+  var $messages = $('.chat-messages');
+  var $msg = $('<div>').addClass('alert').text(msg);
+  if (mine) {
+    $msg.addClass('alert-success');
+  } else {
+    $msg.addClass('alert-info');
+  }
+  $messages.append($msg);
+  $messages.animate({
+    scrollTop: $msg.offset().top
+  }, 500);
+
 }
 
 socket.on('message', function (msg) {
   addMessage(msg);
 });
 
-$('#chat-window input').on('keyup', function (evt) {
+$('.new-message').on('keyup', function (evt) {
   var code = evt.which;
   if (code == 13) {
     var msg = $(this).val();
-    addMessage(msg);
+    addMessage(msg, true);
     socket.emit('send', msg);
     $(this).val('');
   }
